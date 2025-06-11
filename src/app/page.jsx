9fcd2e9fad2d7299eac3150267dd9341124ce1d2
Khashayar Mobarez <khashayarmobarez333@gmail.com>
2025-06-11 12:50:00 +0000

    commiting to the folder

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="flex flex-col min-h-screen bg-background text-foreground">
       <header className="py-6 shadow-sm">
         <div className="container mx-auto px-4">
-          <h1 className="text-3xl font-headline font-bold text-primary text-center md:text-left">Banafsheh Mesripour</h1>
+          <h1 className="text-3xl font-headline font-bold text-primary text-center md:text-left">Banafshe Mesripour</h1>
           <p className="text-center md:text-left text-muted-foreground font-headline">Calligraphy Artist</p>
         </div>
       </header>
